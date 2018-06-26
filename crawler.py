96002e8d805049ecadc52c6c8f188f15598d5364
LuciaVG <bvluly@gmail.com>
2018-06-25 23:27:02 -0500

    finds urls

--- crawler.py
+++ crawler.py
@@ -7,8 +7,6 @@ htmlSource = sock.read()
 sock.close()
 print htmlSource
 
-m = re.findall('href=?(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', htmlSource)
-print m
 
 urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', htmlSource)
-#print("Urls: ",urls)
+print("Urls: ",urls)
